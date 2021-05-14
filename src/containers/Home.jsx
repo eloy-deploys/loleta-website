83389292b4db4116b376b2bdaab94d08c16b9c20
eloy98g <eloygomez132@gmail.com>
2021-05-14 12:49:07 +0200

    first commit

---
+++ src/containers/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/containers/Home.scss';
+
+const Home = () => {
+  
+  
+  return (
+    <div className="home">
+      <h1>ola</h1>
+    </div>
+  );
+};
+
+export default Home;
